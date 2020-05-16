fd5317fea8bbdeef0775d5242d5df18df9b247e8
Agil89 <agil.makhmudov@mail.ru>
2020-05-16 16:10:42 +0400

    last_version

--- codes/services.js
+++ codes/services.js
@@ -1,9 +1,18 @@
 $('.parallax-window').parallax({
-    positionX: '10px',
+    naturalWidth: '500',
+    naturalHeight: '300',
+    positionX: '',
 });
 document.querySelector('.hamburger').addEventListener('click', function() {
     document.querySelector('.inhamburger').classList.toggle('show');
 })
 document.querySelector('.hamburger-x').addEventListener('click', function() {
     document.querySelector('.inhamburger').classList.toggle('show');
+})
+function scrollPage(){
+    jQuery(window).trigger('resize').trigger('scroll');
+
+}
+$(document).ready(function(){
+    scrollPage();
 })
